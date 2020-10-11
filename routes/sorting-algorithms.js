760c8fd0a663fa9c329d1a0adbd198ac35a3ca88
arunsri7 <arun.i1997@gmail.com>
2020-10-11 18:44:36 +0530

    [added more sorting algorithms]

--- routes/sorting-algorithms.js
+++ routes/sorting-algorithms.js
@@ -24,3 +24,27 @@ let BubbleSort = (arr) => {
     
 }
 export {BubbleSort}
+
+
+
+
+function quick_Sorting(array) {
+  if (array.length <= 1) {
+  return array; // if there is only one element then return the same
+  } else
+  {
+  var left = [];
+  var right = [];
+  var outputArray = [];
+  var pivot = array.pop();
+  var length = array.length;
+  for (var i = 0; i < length; i++) {
+  if (array[i] <= pivot) {
+  left.push(array[i]);
+  } else {
+  right.push(array[i]);
+  }
+  }
+  return outputArray.concat(quick_Sorting(left), pivot, quick_Sorting(right));
+  }
+  }
